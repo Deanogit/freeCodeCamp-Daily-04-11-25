2d11f7d09f3b59b13d448316ff9462231413e5b7
Dean Howe <hideanhowe@gmail.com>
2026-01-02 07:47:27 +0000

    filter solution

--- index.js
+++ index.js
@@ -6,14 +6,19 @@
 // Ignore the case when matching the search terms.
 // Return the images in the same order they appear in the input array.
 
-function imageSearch(images, term) {
-  console.log(images, term);
-  const arr = [];
+// function imageSearch(images, term) {
+//   console.log(images, term);
+//   const arr = [];
 
-  for (let i = 0; i < images.length; i++) {
-    if (images[i].toLowerCase().includes(term.toLowerCase())) {
-      arr.push(images[i]);
-    }
-  }
-  return arr;
+//   for (let i = 0; i < images.length; i++) {
+//     if (images[i].toLowerCase().includes(term.toLowerCase())) {
+//       arr.push(images[i]);
+//     }
+//   }
+//   return arr;
+// }
+
+function imageSearch(images, term) {
+  const lowerTerm = term.toLowerCase();
+  return images.filter((img) => img.toLowerCase().includes(lowerTerm));
 }
